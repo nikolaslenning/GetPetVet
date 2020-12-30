4822a4ff9b5a3abb949823ad01d8fbc5388a1dbe
Sydney Goodwill <sydneyjogoodwill@gmail.com>
2020-12-30 14:51:23 -0700

    added carousel with mobile first text

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,5 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import React, { Component } from 'react';
+import pet1 from "../assets/pet1.jpg";
+import patient2 from "../assets/patient2.jpg";
+import doctor3 from "../assets/doctor3.jpg";
+import "./Home.css";
 
 class Home extends Component {
   // constructor() {
@@ -9,7 +13,51 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <p>It's good to be home</p>
+        <h1 id="welcome">Welcome!</h1>
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
+    <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
+    <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={pet1} class="d-block w-100" alt="pet1"></img>
+      <div class="carousel-caption" id="captionTxt">
+                <h5 id="carouselExampleCaptions">Schedule Online</h5>
+      <div class="carousel-caption d-none d-md-block" id="captionTxt">
+        <p>GetPetVet allows you to schedule vet appointments by connecting directly with your veterinarian online.</p>
+                </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src={patient2} class="d-block w-100" alt="patient2"></img>
+      <div class="carousel-caption" id="captionTxt">
+                <h5 id="carouselExampleCaptions">All from Home</h5>
+      <div class="carousel-caption d-none d-md-block" id="captionTxt">
+        <p>Our Virtual Vet Visits are an excellent way to interact with your veterinarian without having to leave the house.</p>
+                </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src={doctor3} class="d-block w-100" alt="doctor3"></img>
+      <div class="carousel-caption" id="captionTxt">
+                <h5 id="carouselExampleCaptions">Quality Care</h5>
+      <div class="carousel-caption d-none d-md-block" id="captionTxt">
+        <p>Connect with the veterinarians you know and trust. Urgent Care doctors are also always on call.</p>
+                </div>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </a>
+</div>
       </div>
     );
 
