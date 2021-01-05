c4a75dea5cfc61e815e574279ab188dea13062a5
Sydney Goodwill <sydneyjogoodwill@gmail.com>
2021-01-04 18:55:00 -0700

    updated store.js and CaldendarForm.js so that week and day view render properly

--- client/src/components/Scheduler/store.js
+++ client/src/components/Scheduler/store.js
@@ -4,7 +4,13 @@ class CalendarStore {
     calendarEvents = [];
 
     setCalendarEvents(calendarEvents) {
-      this.calendarEvents = calendarEvents;
+      this.calendarEvents = calendarEvents.map(d => {
+        return {
+          ...d,
+          start: new Date(d.start),
+          end: new Date(d.end)
+        };
+      });
     }
     constructor() {
       makeObservable(this, {
