1cb2a721381e23164282bbab0709d4b761442011
Slothing <seth.r.martineau@gmail.com>
2021-01-05 20:34:15 -0700

    doctor error fixed, added isDoctor to user schema

--- client/src/components/Doctors/Doctors.js
+++ client/src/components/Doctors/Doctors.js
@@ -2,59 +2,59 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-class DoctorCards extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            doctor: []
-        };
-    }
+class Doctors extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      doctor: []
+    };
+  }
 
-    getDoctor() {
-        axios.get('/doctors')
-            .then(res => {
-                console.log("res.data");
-                console.log(res);
-                console.log(res.data);
-                this.setState({
-                    doctor: res.data.data
-                });
-            })
-            .catch(err => console.log(err));
-    }
+  getDoctor() {
+    axios.get('/doctors')
+      .then(res => {
+        console.log("res.data");
+        console.log(res);
+        console.log(res.data);
+        this.setState({
+          doctor: res.data.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
 
-    componentDidMount() {
-        this.getDoctor();
-    }
+  componentDidMount() {
+    this.getDoctor();
+  }
 
-    render() {
-        return (
-            <div>
-                <h3>Doctor Profiles</h3>
-                {console.log(this.state.doctor)}
-                {this.state.doctor.map(doctor =>
-                    <div key={doctor._id} className="card text-center">
-                        <div className="card-header">
-                            <img src={doctor.imageLinks} alt={doctor.title} />
-                        </div>
-                        <div className="card-body">
-                            <h2>Dr. {doctor.firstName} {doctor.lastName}</h2>
-                            <p>Address: {doctor.address}</p>
-                            <p>Province: {doctor.province}</p>
-                            <p>Zip Code: {doctor.zipCode}</p>
-                            <p>Phone Number: {doctor.phoneNumber}</p>
-                            <p>Email: {doctor.email}</p>
-                            {/* <a href="{doctor.email}" target="_blank" rel="noreferrer" >Book Info</a>
+  render() {
+    return (
+      <div>
+        <h3>Doctor Profiles</h3>
+        {console.log(this.state.doctor)}
+        {this.state.doctor.map(doctor =>
+          <div key={doctor._id} className="card text-center">
+            <div className="card-header">
+              <img src={doctor.imageLinks} alt={doctor.title} />
+            </div>
+            <div className="card-body">
+              <h2>Dr. {doctor.firstName} {doctor.lastName}</h2>
+              <p>Address: {doctor.address}</p>
+              <p>Province: {doctor.province}</p>
+              <p>Zip Code: {doctor.zipCode}</p>
+              <p>Phone Number: {doctor.phoneNumber}</p>
+              <p>Email: {doctor.email}</p>
+              {/* <a href="{doctor.email}" target="_blank" rel="noreferrer" >Book Info</a>
                             <br></br>
                             <button className="btn btn-primary" onClick={onClick}>Save</button> */}
-                        </div>
-                    </div>
-                )
-                }
             </div>
+          </div>
+        )
+        }
+      </div>
 
-        );
-    }
+    );
+  }
 }
 
-export default DoctorCards;
+export default Doctors;
