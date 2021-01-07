4597fe4e62121a1cca6f737b91ddf36e1c488ebc
Zach LaFleur <zach.lafleur95@gmail.com>
2021-01-07 13:49:14 -0700

    added more information to the doctor seed. added that same information to the doctor card.

--- server/seeds/seedDB.js
+++ server/seeds/seedDB.js
@@ -13,7 +13,11 @@ const doctorSeed = [
       address: "345 Creepy Castle Rd",
       province: "Transylvania, RO",
       zipCode: 500326,
-      phoneNumber: 403592513588
+      phoneNumber: 403592513588,
+      about: "Dr. Frankenstien has been working with pets for years and has grown to love all animals so much. He is family friendly and will only do his best to insure the proper care for your pets.",
+      schedule: "Monday - Friday 9:00 am - 5:00 pm",
+      facility: "Franken Pets Vets",
+      image: ""
     },
     {
       isDoctor: true,
@@ -24,7 +28,11 @@ const doctorSeed = [
       address: "2011 Royal Oak Rd",
       province: "Pontiac, MI",
       zipCode: 48302,
-      phoneNumber: 124812354165
+      phoneNumber: 124812354165,
+      about: "Dr. Kevorkian has been working in the health industry for many years, and always places the patients well being first. He will ensure your pet gets exactly what they want and need.",
+      schedule: "Monday - Friday 10:00 am - 6:00 pm",
+      facility: "Kevorkian all Pets",
+      image: ""
     }
   ];
   Doctor.remove({})
