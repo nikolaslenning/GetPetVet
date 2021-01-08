702904326008cd9a55ddb4b552e514c080fc993e
Zach LaFleur <zach.lafleur95@gmail.com>
2021-01-08 13:33:22 -0700

    moved old videos to old video file. started set up on new chat/video capabilities. added new routes on navbar as well as app.js.

--- client/src/components/VideoChat/VideoChat.js
+++ client/src/components/VideoChat/VideoChat.js
@@ -1,174 +1,80 @@
-/* eslint-disable no-unused-vars */
-import React, { useEffect, useState, useRef } from 'react';
-import './VideoChat.css';
-import io from "socket.io-client";
-import Peer from "simple-peer";
+// eslint-disable-next-line no-unused-vars
+import React, { Component } from "react";
+import axios from "axios";
 
+function onClick(e) {
+    e.preventDefault();
+    axios.post("/scheduler", {
 
-
-function NameSelf({
-  setName,
-}) {
-  const inputRef = useRef();
-
-  function onClick() {
-    setName(inputRef.current.value);
-  }
-
-  return (
-    <div> Please Enter Your Name
-      <input id="caller-name" type="text" placeholder="your name goes here." ref={inputRef} />
-      <button onClick={onClick} >Submit</button>
-    </div>
-  );
+    });
 }
 
-function Stream(prop) {
-  const [yourID, setYourID] = useState("");
-  const [users, setUsers] = useState({});
-  const [stream, setStream] = useState();
-  const [receivingCall, setReceivingCall] = useState(false);
-  const [caller, setCaller] = useState("");
-  const [callerSignal, setCallerSignal] = useState();
-  const [callAccepted, setCallAccepted] = useState(false);
-  // const [callerName, setCallerName] = useState(null);
-  const [initiatorName, setInitiatorName] = useState(null);
+class videoChat extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: "",
+            firstName: "",
+            lastName: ""
+        };
+    }
+    render () {
+        return(
 
-  const userVideo = useRef();
-  const partnerVideo = useRef();
-  const socket = useRef();
+            <section className="hero light is-fullheight">
+        <div className="hero-body">
+            <div className="container">
+                <div className="columns is-centered">
+                    <div className="column  is-5-tablet is-4-desktop is-3-widescreen has-text-centered">
+                        <span className="icon is-large">
+                            <i className="fa fa-comments"></i>
+                        </span>
+                        <h2 className="title is-3"> VIDEO CHAT ROOMS</h2>
+                    </div>
+                </div>
+                <div className="columns is-centered">
 
-  useEffect(() => {
-    socket.current = io.connect("/");
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
-      setStream(stream);
-      if (userVideo.current) {
-        userVideo.current.srcObject = stream;
-      }
-    });
-
-    socket.current.on("yourID", (id) => {
-      setYourID(id);
-    });
-
-    socket.current.on("allUsers", (users) => {
-      setUsers(users);
-    });
-
-    socket.current.on("hey", (data) => {
-      console.log(data);
-      setReceivingCall(true);
-      setCaller(data.from);
-      setCallerSignal(data.signal);
-    });
-  }, []);
-
-  useEffect(() => {
-    socket.current.emit('nameSelf', { id: yourID, username: initiatorName });
-  }, [initiatorName]);
-
-  function callPeer(id) {
-    const peer = new Peer({
-      initiator: true,
-      trickle: false,
-      stream: stream,
-    });
-
-    peer.on("signal", data => {
-      socket.current.emit("callUser", { userToCall: id, signalData: data, from: yourID });
-    });
-
-    peer.on("stream", stream => {
-      if (partnerVideo.current) {
-        partnerVideo.current.srcObject = stream;
-      }
-    });
-
-    socket.current.on("callAccepted", signal => {
-      setCallAccepted(true);
-      peer.signal(signal);
-    });
-  }
-
-  function acceptCall() {
-    setCallAccepted(true);
-    const peer = new Peer({
-      initiator: false,
-      trickle: false,
-      stream: stream,
-    });
-    peer.on("signal", data => {
-      socket.current.emit("acceptCall", { signal: data, to: caller });
-    });
-
-    peer.on("stream", stream => {
-      partnerVideo.current.srcObject = stream;
-    });
-
-    peer.signal(callerSignal);
-  }
-
-  let UserVideo;
-  if (stream) {
-    UserVideo = (
-      <video className='video' playsInline muted ref={userVideo} autoPlay />
+                    <div className="column is-5-tablet is-4-desktop is-3-widescreen">
+                        <form action="/videochat" method="POST" className="box">
+                            <div className="field">
+                                <label className="label">Username</label>
+                                <h1>{this.props.firstName}</h1>
+                                <div className="control has-icons-left">
+                                    <p name="username">{this.props.firstName} {this.props.lastName}</p>
+                                    <span className="icon is-small is-left">
+                                        <i className="fa fa-user"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <div className="field">
+                                <label className="label">Select Room Name</label>
+                                <p className="control has-icons-left">
+                                    <select name="room" className="roomselect" aria-placeholder="Room Name" >
+                                        <option value="Kevorkian">Kevorkian</option>
+                                        <option value="Frank">Frankie</option>
+                                    </select>
+                                    {/* <!-- <input type="text" placeholder="Room Name" className="input" name="room" required> --></input> */}
+                                    <span className="icon is-small is-left">
+                                      <i className="fas fa-comment-alt"></i>
+                                    </span>
+                                </p>
+                            </div>
+                            <div className="field ">
+                                <button onClick={onClick} className="button is-link">
+                                    <span className="icon is-small is-left mr-1">
+                                        <i className="fas fa-person-booth"></i>
+                                      </span>
+                                    JOIN
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     );
-  }
-
-  let PartnerVideo;
-  if (callAccepted) {
-    PartnerVideo = (
-      <video className='video' playsInline ref={partnerVideo} autoPlay />
-    );
-  }
-
-  let incomingCall;
-  if (receivingCall) {
-    incomingCall = (
-      <div>
-        <h1>{prop.username} is calling you</h1>
-        <button onClick={acceptCall}>Accept</button>
-      </div>
-    );
-  }
-
-
-  function handleInitiatorname(value) {
-    setInitiatorName(value);
-  }
-
-  // function handleCallerName(target) {
-  //   setCallerName(target.value)
-  // }
-
-  return (
-    <container className="container">
-      <row className="row">
-        {!initiatorName && (<NameSelf setName={handleInitiatorname} />)}
-        <column>
-          <h1>{prop.username}</h1>
-          {UserVideo}
-        </column>
-        <column>
-          {/* <h1>{callerName}</h1> */}
-          {PartnerVideo}
-        </column>
-      </row >
-      <row className="row">
-        {Object.keys(users).map(key => {
-          if (key === yourID) {
-            return null;
-          }
-          return (
-            <button onClick={() => callPeer(key)}>Call {prop.username}</button>
-          );
-        })}
-      </row >
-      <row className="row">
-        {incomingCall}
-      </row >
-    </container>
-  );
+    }
 }
 
-export default Stream;
+export default videoChat;
