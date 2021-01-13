7140aa23dc3b2289cf690d5928f4f5fd9e6d4490
Nikolas Lenning <nikolaslenning@gmail.com>
2021-01-13 12:25:51 -0700

    init

--- server/routes/api/videochat.js
+++ server/routes/api/videochat.js
@@ -1,12 +1,13 @@
+/* eslint-disable no-unused-vars */
 const express = require("express");
 const router = express.Router();
 
 router
     .route("/")
     .get((req, res) => {
-        console.log("+++ HIT VIDEO CHAT +++");
-        console.log(res);
-        console.log(req);
+        // console.log("+++ HIT VIDEO CHAT +++");
+        // console.log(res);
+        // console.log(req);
     });
 
     module.exports = router;
--- server/server.js
+++ server/server.js
@@ -7,9 +7,7 @@ const server = http.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
 const routes = require('./routes');
-// const formatMessage = require('./utils/messages');
-// const { userJoin, getRoomUsers, getUser, userLeave } = require('./utils/users');
-// const calendarRoutes = require('./routes/calendar');
+
 
 const path = require('path');
 // const users = require("./models/user");
@@ -37,85 +35,7 @@ app.use(passport.session()); // calls the deserializeUser
 
 // routes
 app.use(routes);
-// app.use('/scheduler', calendarRoutes);
-// require("./routes/user")(app);
-// require("./routes/calendar")(app);
 
-
-// const exApp = express();
-// const server = http.createServer(exApp);
-
-// const io = socketio(server);
-
-
-// const peerServer = ExpressPeerServer(server, { debug: true });
-
-// exApp.use('/peerjs', peerServer)
-
-// set static folder
-// exApp.use(express.static(path.join(__dirname, 'public')));
-
-// const botName = 'Chat Bot';
-
-// io.on('connection', socket => {
-//   socket.on('join-room', ({ firstName, userName, facility }) => {
-//     const user = userJoin(firstName, userName, facility);
-
-//     socket.join(user.room);
-
-//     socket.emit('message', formatMessage(botName, "Welcome to GetPetVet!"));
-//     socket.emit('enter-room', user.room);
-
-//     // Broadcast when  a user connects
-//     socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat, be nice with him/her`));
-
-//     // Send users and Room info
-//     io.to(user.room).emit('room-users', {
-//       room: user.room,
-//       users: getRoomUsers(user.room)
-//     });
-
-//     // Listen for chatMessage
-//     socket.on('user-message', msg => {
-//       const user = getUser(id);
-//       io.to(user.room).emit('message', formatMessage(user.username, msg));
-
-//     });
-
-
-//     // Runs when client disconnects
-//     socket.on('disconnect', () => {
-//       const user = userLeave(id);
-
-//       if (user) {
-//         io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
-
-//         // Send users and room info
-//         io.to(user.room).emit('room-users', {
-//           room: user.room,
-//           users: getRoomUsers(user.room)
-//         });
-//       }
-//     });
-
-//     // Listen to WebcamOn
-//     socket.on('webcam-on', () => {
-//       user.cam = true;
-//       io.to(user.room).emit('add-webcam-icon', user.id);
-//     });
-
-//     // Listen to webcamOff
-//     socket.on('webcam-off', () => {
-//       user.cam = false;
-//       io.to(user.room).emit('remove-webcam-icon-stream-called', user.id);
-//     });
-//   });
-
-// });
-
-// const PORT = process.env.PORT || 3000;
-
-// server.listen(PORT, () => console.log(`Server running on http://127.0.0.1:${PORT}`));
 
 // sockets old SOCKET IS CLIENT, IO IS THE SERVER
 // io.sockets is all clients
